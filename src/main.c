e001e4a524553695db87b14057e3418ab70850f7
DajanaS <dajana.sk@hotmail.com>
2018-05-04 13:13:47 +0200

    Init project

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_i2c.h"
+#include "lpc17xx_ssp.h"
+#include "lpc17xx_adc.h"
+#include "lpc17xx_timer.h"
+
+#include "oled.h"
+
+
+int main (void) {
+	//
+}
